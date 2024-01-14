5b51510090139c8583342a3c36abf649e82cc997
Hanzala Yousuf <hanzalayousuf159@gmail.com>
2024-01-14 13:53:22 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,53 @@
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
+});
+
+
+// faqs hide and show//
+
+
+const faqs = document.querySelectorAll('.faq');
+
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        // change icon
+
+        const icon = faq.querySelector('.faq-icon i')
+        if(icon.className === 'bx bx-plus') {
+            icon.className = 'bx bx-minus';
+        } else{
+            icon.className = 'bx bx-plus';
+        }
+    });
+});
+
+
+
+// nav hide and show
+
+
+const menu = document.querySelector(".nav-menu")
+const menuBtn = document.querySelector("#open-menu-btn")
+const closeBtn = document.querySelector("#close-menu-btn")
+
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+});
+
+
+
+
+const closeNav = () =>{
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+
+closeBtn.addEventListener('click', closeNav);
